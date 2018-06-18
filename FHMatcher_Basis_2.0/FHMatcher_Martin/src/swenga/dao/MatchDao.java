a51de09f5e462d89f176246e21c39c9e4e17b63e
wallnerbar <barbara.wallner2@edu.fh-joanneum.at>
2018-06-18 09:54:03 +0200

    FH Matcher Basis Version noch einmal hochladen

---
+++ FHMatcher_Basis_2.0/FHMatcher_Martin/src/swenga/dao/MatchDao.java
@@ -0,0 +1,19 @@
+package swenga.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+ 
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import swenga.model.MatchesModel;
+
+@Repository
+@Transactional
+public class MatchDao {
+
+}
